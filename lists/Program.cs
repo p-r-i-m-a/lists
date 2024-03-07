8daff4a5ba0e4e2529b0aa767f603d9284f61761
Domenic Maniacco <dmaniacco3291@gmail.com>
2024-03-07 14:39:19 -0500

    uhh

--- lists/Program.cs
+++ lists/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            List <int> numbers = new List<int>();
+            Random generator = new Random();  
+
+            for (int i = 0; i <= 25; i++)
+            {
+                numbers.Add(generator.Next(1, 25));
+
+                Console.WriteLine(numbers[i]);
+            }
+
+
         }
     }
 }
